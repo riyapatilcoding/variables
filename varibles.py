4a58a4cbdc75349be4731b6e970e1e8d49b27428
riyapatilcoding <patil_raj8@yahoo.com>
2026-07-23 17:16:24 -0400

    Add files via upload

---
+++ varibles.py
@@ -0,0 +1,12 @@
+print('registration form')
+name=input('What is your name')
+print(name)
+
+age=input('What is your age')
+print(age)
+
+city=input('What city do you live in')
+print(city)
+
+school=input('What school do you go to')
+print(school)
